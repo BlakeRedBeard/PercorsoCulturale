d0f454ed8da8543fdbf6656847c06c2feabbb61e
lorenzotermite <lorenzotermite99@gmail.com>
2022-12-03 14:35:05 +0100

    Creato activity_profilo.xml, collegamento tra home e profilo tramite il menu

---
+++ app/src/main/java/com/example/percorsoculturale/BottomoSheetActivity.java
@@ -0,0 +1,16 @@
+package com.example.percorsoculturale;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class BottomoSheetActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.bottom_sheet);
+
+
+    }
+}
