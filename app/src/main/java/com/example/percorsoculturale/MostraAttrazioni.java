ceafe6a3f0bc74d92fc0e0bd8d24b771549c7e21
Davide <davidett98@gmail.com>
2022-12-06 10:03:31 +0100

    Inserito metodo che consente di verificare se l'attività è presente nell'attrazione

--- app/src/main/java/com/example/percorsoculturale/MostraAttrazioni.java
+++ app/src/main/java/com/example/percorsoculturale/MostraAttrazioni.java
@@ -1,5 +1,7 @@
 package com.example.percorsoculturale;
 
+import static android.content.ContentValues.TAG;
+
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -19,6 +21,7 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.storage.FirebaseStorage;
@@ -67,6 +70,7 @@ public class MostraAttrazioni extends AppCompatActivity {
                    //TODO generare eccezione (non è possibile identificare l'attrazione)
                }
                showAttrazione(attrazioni.get(id));
+               checkAttivita(attrazioni.get(id));
                 //Inizializzazione bottoni
                btnAvanti.setOnClickListener(new View.OnClickListener() {
                    @Override
@@ -151,6 +155,43 @@ public class MostraAttrazioni extends AppCompatActivity {
                     }
                 });
 
+    }
+
+    public void checkAttivita(String search) {
+        db.collection("attrazione")
+                .document(search)
+                .get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    int valore = 0;
+                    if (document.exists()) {
+                        Log.d(TAG, "DocumentSnapshot data: " + document.getData());
+
+                        String attivita = document.getString("attivita");
+
+
+                        if (attivita == ""){
+                            valore = 0;//controlla se non è vuoto
+
+                            if (valore == 1) {
+                                btnAttivita.setVisibility(View.VISIBLE);
+                            }
+                            else {
+                                btnAttivita.setVisibility(View.INVISIBLE);
+                            }
+                        }
+
+
+                    } else {
+                        Log.d(TAG, "No such document");
+                    }
+                } else {
+                    Log.d(TAG, "get failed with ", task.getException());
+                }
+            }
+        });
     }
 
 }
