2e762911cedf83c906fab59dc56581ea1af85b08
lorenzotermite <lorenzotermite99@gmail.com>
2022-12-28 14:14:08 +0100

    Aggiunta funzionalità rilevamento posizione

--- app/src/main/java/com/example/percorsoculturale/RicercaPercorsiActivity.java
+++ app/src/main/java/com/example/percorsoculturale/RicercaPercorsiActivity.java
@@ -1,6 +1,14 @@
 package com.example.percorsoculturale;
 
+import static com.google.zxing.integration.android.IntentIntegrator.REQUEST_CODE;
+
+import android.Manifest;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
@@ -12,32 +20,29 @@ import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.SearchView;
 import android.widget.Toast;
-import android.content.DialogInterface;
-import androidx.appcompat.app.AlertDialog;
-import androidx.annotation.NonNull;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import com.example.percorsoculturale.tables.Percorso;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.bottomsheet.BottomSheetBehavior;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.QueryDocumentSnapshot;
-import com.google.firebase.firestore.QuerySnapshot;
 
-import org.json.JSONArray;
-import org.json.JSONObject;
-
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Map;
+import java.util.List;
+import java.util.Locale;
 
 public class RicercaPercorsiActivity extends AppCompatActivity {
 
@@ -53,7 +58,8 @@ public class RicercaPercorsiActivity extends AppCompatActivity {
     private BottomSheetBehavior mBottomSheetBehavior;
     private BottomSheetBehavior BottomSheetBehavior;
     private FirebaseAuth firebaseAuth;
-
+    private FusedLocationProviderClient fusedLocationClient;
+    private Geocoder geocoder;
 
 
     @Override
@@ -172,7 +178,79 @@ public class RicercaPercorsiActivity extends AppCompatActivity {
                 }
             });
         }
+//GPS
+        fusedLocationClient= LocationServices.getFusedLocationProviderClient(this);
+     getLocation();
 
+    }
+
+
+
+    private void getLocation() {
+        if(PackageManager.PERMISSION_GRANTED== ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)){
+
+            Toast.makeText(this,"Permesso gia accettato",Toast.LENGTH_LONG).show();
+            fusedLocationClient.getLastLocation()
+                    .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                        @Override
+                        public void onSuccess(Location location) {
+                            if (location != null) {
+                            geocoder=new Geocoder(RicercaPercorsiActivity.this, Locale.getDefault());
+                                try {
+                                    Toast.makeText(RicercaPercorsiActivity.this,"Entrato",Toast.LENGTH_LONG).show();
+                                    List<Address> addresses=geocoder.getFromLocation(location.getLatitude(),location.getLongitude(),1);
+                                    //TODO Passare la regione a MostraPercorsi
+                                    String Regione=addresses.get(0).getAdminArea();
+                                    Toast.makeText(RicercaPercorsiActivity.this,Regione,Toast.LENGTH_LONG).show();
+                                } catch (IOException e) {
+                                    e.printStackTrace();
+                                }
+
+                            }
+                        }
+                    });
+
+
+        }else if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_COARSE_LOCATION)) {
+
+            new AlertDialog.Builder(this)
+                    .setTitle("Permesso posizione")
+                    .setMessage("Accettando il permesso cercheremo i percorsi vicini alla tua posizione, non è obbligatoria ma facilita l'utilizzo dell'app")
+                    .setPositiveButton("ok", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            ActivityCompat.requestPermissions(RicercaPercorsiActivity.this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},REQUEST_CODE);
+                        }
+                    })
+                    .setNegativeButton("Non accetto", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+
+                        }
+                    })
+                    .create().show();
+        } else {
+            // You can directly ask for the permission.
+            // The registered ActivityResultCallback gets the result of this request.
+          ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},REQUEST_CODE);
+        }
+
+
+
+    }
+    //gps
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if(requestCode==REQUEST_CODE){
+            if(grantResults.length>0 &&grantResults[0] ==PackageManager.PERMISSION_GRANTED)
+            {
+            getLocation();
+            }else{
+                Toast.makeText(this,"Permesso negato", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 
     private void showMessage(String messaggio) {
