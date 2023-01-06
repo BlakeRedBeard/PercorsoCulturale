89b336e65f10cea7b3ec2578a9063e807e78dfa6
Davide <davidett98@gmail.com>
2023-01-06 16:08:54 +0100

    Implementata l activity per recuperare la password

--- app/src/main/java/com/example/percorsoculturale/RecuperaPassword.java
+++ app/src/main/java/com/example/percorsoculturale/RecuperaPassword.java
@@ -2,63 +2,66 @@ package com.example.percorsoculturale;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.percorsoculturale.databinding.ActivityRecuperaPasswordBinding;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.Task;
-import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
 import com.google.firebase.auth.FirebaseAuth;
 
-public class RecuperaPassword extends AppCompatActivity{
-
+public class RecuperaPassword extends AppCompatActivity {
 
     private TextInputLayout emailView;
     private Button btnReset;
+    FirebaseAuth auth = FirebaseAuth.getInstance();
 
 
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recupera_password);
+
         emailView = (TextInputLayout) findViewById(R.id.editTextTextEmailAddress);
+        if (savedInstanceState != null) {
+            emailView.getEditText().setText(savedInstanceState.getString("email"));
+        }
+
+
         btnReset = (Button) findViewById(R.id.loginButton);
 
-    btnReset.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
 
-            String mail = ((TextInputEditText) findViewById(R.id.iscrivitiEmail)).getText().toString().trim().toLowerCase();
-            FirebaseAuth auth = FirebaseAuth.getInstance();
-
-            if (TextUtils.isEmpty(mail)) {
-                Toast.makeText(getApplication(), "U sce scriv apprim", Toast.LENGTH_SHORT).show();
-                return;
-            }
-
-            auth.sendPasswordResetEmail(mail)
-                    .addOnCompleteListener(new OnCompleteListener<Void>() {
-                        @Override
-                        public void onComplete(@NonNull Task<Void> task) {
-                            if (task.isSuccessful()) {
-                                Toast.makeText(RecuperaPassword.this, "Awand la password", Toast.LENGTH_SHORT).show();
-                                Intent i = new Intent(getApplicationContext(), LoginActivity.class);
-                                startActivity(i);
-                            }
-
+        btnReset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String mail = emailView.getEditText().getText().toString().trim().toLowerCase();
+                System.out.println("DEBUG: mail formattata: "+mail);
+                auth.sendPasswordResetEmail(mail).addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(RecuperaPassword.this, "Email inviata", Toast.LENGTH_LONG).show();
+                            Intent i = new Intent(getApplicationContext(), LoginActivity.class);
+                            startActivity(i);
+                        } else {
+                            Toast.makeText(RecuperaPassword.this, "Questa email non è registrata", Toast.LENGTH_SHORT).show();
                         }
-                    });
-        }
-    });
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(RecuperaPassword.this, "Error", Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
 
 
     }
-
 }
