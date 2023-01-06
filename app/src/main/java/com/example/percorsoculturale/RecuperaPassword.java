b8b897027fada38ba0c40a5075d2ebd21defa628
Davide <davidett98@gmail.com>
2023-01-06 16:25:37 +0100

    Aggiunto multilingua alla mail di recupero password

--- app/src/main/java/com/example/percorsoculturale/RecuperaPassword.java
+++ app/src/main/java/com/example/percorsoculturale/RecuperaPassword.java
@@ -1,6 +1,8 @@
 package com.example.percorsoculturale;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -27,6 +29,11 @@ public class RecuperaPassword extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recupera_password);
+
+        SharedPreferences pref = getSharedPreferences("Settings", Activity.MODE_PRIVATE);
+        String language = pref.getString("My_Lang", "en").equals("") ? "it" : pref.getString("My_Lang", "en");
+
+        auth.setLanguageCode(language);
 
         emailView = (TextInputLayout) findViewById(R.id.editTextTextEmailAddress);
         if (savedInstanceState != null) {
